0378132842418dc1523eb2451bf7ab5d3322aa16
VovaPrymachuk <vovaprimachuk@gmail.com>
2021-08-18 11:47:37 +0300

    created delete post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,25 +18,34 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-    if(@post.update(post_params))
+    if @post.update(post_params)
       redirect_to @post
     else
       render 'edit'
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+
+    @post.destroy
+    redirect_to posts_path
+  end
+
   def create
     # render plain: params[:post].inspect
     @post = Post.new(post_params)
 
-    if(@post.save)
+    if @post.save
       redirect_to @post
     else
       render 'new'
     end
   end
 
-  private def post_params
+  private
+
+  def post_params
     params.require(:post).permit(:title, :body)
   end
 end
